a5d36811544c5dea7d88c7b4890542184d0f11ea
absmugz <absmugz09@gmail.com>
2016-02-18 15:58:52 +0200

    saving status into database

--- application/controllers/Jquizme.php
+++ application/controllers/Jquizme.php
@@ -1,91 +1,94 @@
-<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+<?php
+
+if (!defined('BASEPATH'))
+    exit('No direct script access allowed');
 
 class Jquizme extends CI_Controller {
 
-	public function __construct()
-	{
-		parent::__construct();
-                
-               function create_zip($files = array(),$destination = '',$overwrite = false) {
-	//if the zip file already exists and overwrite is false, return false
-	if(file_exists($destination) && !$overwrite) { return false; }
-	//vars
-	$valid_files = array();
-	//if files were passed in...
-	if(is_array($files)) {
-		//cycle through each file
-		foreach($files as $file) {
-			//make sure the file exists
-			if(file_exists($file)) {
-				$valid_files[] = $file;
-			}
-		}
-	}
-	//if we have good files...
-	if(count($valid_files)) {
-		//create the archive
-		$zip = new ZipArchive();
-		if($zip->open($destination,$overwrite ? ZIPARCHIVE::OVERWRITE : ZIPARCHIVE::CREATE) !== true) {
-			return false;
-		}
-		//add the files
-		foreach($valid_files as $file) {
-			$zip->addFile($file,$file);
-		}
-		//debug
-		//echo 'The zip archive contains ',$zip->numFiles,' files with a status of ',$zip->status;
-		
-		//close the zip -- done!
-		$zip->close();
-//check to make sure the file exists
-		return file_exists($destination);
-	}
-	else
-	{
-		return false;
-	}
-} 
-	}
+    public function __construct() {
+        parent::__construct();
 
-	public function index()
-	{
-            
+        function create_zip($files = array(), $destination = '', $overwrite = false) {
+            //if the zip file already exists and overwrite is false, return false
+            if (file_exists($destination) && !$overwrite) {
+                return false;
+            }
+            //vars
+            $valid_files = array();
+            //if files were passed in...
+            if (is_array($files)) {
+                //cycle through each file
+                foreach ($files as $file) {
+                    //make sure the file exists
+                    if (file_exists($file)) {
+                        $valid_files[] = $file;
+                    }
+                }
+            }
+            //if we have good files...
+            if (count($valid_files)) {
+                //create the archive
+                $zip = new ZipArchive();
+                if ($zip->open($destination, $overwrite ? ZIPARCHIVE::OVERWRITE : ZIPARCHIVE::CREATE) !== true) {
+                    return false;
+                }
+                //add the files
+                foreach ($valid_files as $file) {
+                    $zip->addFile($file, $file);
+                }
+                //debug
+                //echo 'The zip archive contains ',$zip->numFiles,' files with a status of ',$zip->status;
+                //close the zip -- done!
+                $zip->close();
+//check to make sure the file exists
+                return file_exists($destination);
+            } else {
+                return false;
+            }
+        }
+
+    }
+
+    public function index() {
+
+        //$data['main_content'] = 'jquizme';
+        $this->load->view('jquizme');
+    }
+
+    public function zip() {
+
         $this->load->model('Quiz_model', 'quiz');
         //var_dump($this->quiz->get_all());die();
-        
-     
-    $emparray = json_encode($this->quiz->get_all());
-    
-    
-    
-     //write to json file
-    $fp = fopen('empdata.js', 'w');
-    fwrite($fp, " var quiz = { multiList: [{" . $emparray . "}]");
-    fclose($fp);
-    
 
-    
-    
-    $files_to_zip = array(
-	'public/js/jQuizMe-2.2.1.min.js',
-	'public/css/jQuizMe.css',
-        'empdata.js'
-	//'empdata.json'
-);
-    
+
+        $emparray = json_encode($this->quiz->get_all());
+
+
+
+        //write to json file
+        $fp = fopen('empdata.js', 'w');
+        fwrite($fp, " var quiz = { multiList: [{" . $emparray . "}]");
+        fclose($fp);
+
+
+
+
+        $files_to_zip = array(
+            'public/js/jQuizMe-2.2.1.min.js',
+            'public/css/jQuizMe.css',
+            'empdata.js'
+                //'empdata.json'
+        );
+
 //if true, good; if false, zip creation failed
-$result = create_zip($files_to_zip,'my-archive.zip');
+        $result = create_zip($files_to_zip, 'my-archive.zip');
 
-    var_dump($emparray);die();
-    //var_dump($emparray);die();
-        
-	$data['main_content'] = 'jquizme';
+        var_dump($emparray);
+        die();
+        //var_dump($emparray);die();
+
+        $data['main_content'] = 'jquizme';
         $this->load->view('includes/template', $data);
-	}
-        
-
-
-
-
+    }
 
 }
--- application/views/jquizme.php
+++ application/views/jquizme.php
@@ -1,4 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Bootstrap 101 Template</title>
 
+<!-- Bootstrap css -->
+
+<link rel="stylesheet" href="<?php echo base_url("public/css/jQuizMe.css"); ?>">
+
+<link rel="stylesheet" href="<?php echo base_url("public/css/style.css"); ?>">
+ </head>
+ <body>
 <div id="quizArea"/>
+
+
+</body>
+<script type="text/javascript" src="<?php echo base_url("public/js/jquery.js"); ?>"></script>
+<script type="text/javascript" src="<?php echo base_url("public/js/jQuizMe-2.2.1.min.js"); ?>"></script>
+<script type="text/javascript">
+$( function($){
+	var quiz = [
+        { 
+                ques: "Silver", //question
+                ans: "Ag", // answer
+                ansInfo : "More info <a href='http://www.periodictable.com/Elements/079/index.html'>here</a>"  // answer information.
+        },
+        {
+                ques: "Gold.", 
+                ans: "Au", 
+                ansInfo : "Gold-Au <br/>More info <a href='http://www.periodictable.com/Elements/079/index.html'>here</a>"
+        }
+	];
+	var options = {
+			addToEnd: " has what periodic symbol ?",
+			quizType: "fill"  //Or you can write fillInTheBlank instead of fill.
+	};
+	// quiz has 2 problems, while the options has set the quiz type to fill in the blank.
+	
+	options.statusUpdate = function( info, $questions ){
+	//$( "#score-right" ).html( info.numOfRight ); 
+	//$( "#score-wrong" ).html( info.numOfWrong ); 
+	//console.log(info.currIndex);
+	console.log(info.total); 
+	console.log(info.numOfRight);
+	console.log(info.numOfWrong); 
+	
+	}
+	$( "#quizArea" ).jQuizMe( quiz, options );
+	
+	//Display Version information.
+	$( "#jqueryVersion" ).text( $.fn.jquery );
+	$( "#jQuizMeVersion" ).text( $.fn.jQuizMe.version );
+});
+</script>
+</html>
 
 
