e7b181a15777a6e2608baa017844771219d126cc
absmugz <absmugz09@gmail.com>
2016-02-26 15:02:16 +0200

    setting up template

---
+++ application/controllers/User.php
@@ -0,0 +1,27 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class User extends MY_Controller {
+	
+	function __construct()
+    {
+        parent::__construct();
+		$this->load->library('ion_auth');
+	}
+
+    public function index()
+    {
+        $this->load->view('welcome_message');
+    }
+
+    public function login()
+    {
+       $this->data['message'] = 'here will be the login form';
+       $this->render('user/login_view');
+    }
+
+    public function logout()
+    {
+        echo 'here we will do the logout';
+    }
+}
