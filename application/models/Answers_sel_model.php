b9c04bdf139c61b2d64a9601fa03eb86c420296f
absmugz <absmugz09@gmail.com>
2016-03-08 10:04:59 +0200

    questions CRUD

---
+++ application/models/Answers_sel_model.php
@@ -0,0 +1,16 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+class Answers_sel_model extends MY_Model
+{
+ public $table = 'answers_sel';
+ 
+ 
+}
+
+
--- application/models/Questions_model.php
+++ application/models/Questions_model.php
@@ -9,4 +9,12 @@
 class Questions_model extends MY_Model
 {
  public $table = 'questions';
+ 
+function __construct()
+{
+        parent::__construct();
+        $this->has_one['answers'] = array('foreign_model'=>'Answers_model','foreign_table'=>'answers','foreign_key'=>'questions_id','local_key'=>'id');
+        $this->has_one['answers_info'] = array('foreign_model'=>'Answers_info_model','foreign_table'=>'answers_info','foreign_key'=>'questions_id','local_key'=>'id');
+        $this->has_many['answers_sel'] = array('foreign_model'=>'Answers_sel_model','foreign_table'=>'answers_sel','foreign_key'=>'questions_id','local_key'=>'id');
+}
 }
